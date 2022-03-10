3000e292551c80c7d4308100810dfe1069bc351c
Space-Belt <dngurwn@gmail.com>
2022-03-10 21:02:52 +0900

    [Feat] 별점 별 추가

---
+++ src/Image.js
@@ -0,0 +1,7 @@
+import Star1 from './assets/star.png';
+import Star2 from './assets/star-1.png';
+import Star3 from './assets/star-2.png';
+import Star4 from './assets/star-3.png';
+import Star5 from './assets/star-4.png';
+
+export { Star1, Star2, Star3, Star4, Star5 };
--- src/components/addReview/Stars.jsx
+++ src/components/addReview/Stars.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Star1, Star2, Star3, Star4, Star5 } from '../../Image.js';
 
 function Stars() {
   return (
     <>
       <Star>
         <Rating>평점</Rating>
+        <StarRating>
+          <img src={Star1} alt="Star1" />
+          <img src={Star2} alt="Star1" />
+          <img src={Star3} alt="Star1" />
+          <img src={Star4} alt="Star1" />
+          <img src={Star5} alt="Star1" />
+        </StarRating>
       </Star>
     </>
   );
@@ -20,6 +28,17 @@ const Star = styled.div`
 const Rating = styled.h1`
   width: 100%;
   font-size: 2rem;
+`;
+
+const StarRating = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  margin: 2rem auto;
+  & img {
+    width: 3rem;
+    height: 3rem;
+  }
 `;
 
 export default Stars;
